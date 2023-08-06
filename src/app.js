bd3e77e0a0e6ea77575aa0d2a6d801cab570bf83
HenriqueSilva <HenriqueSilva254@gmail.com>
2023-08-06 16:08:31 -0300

    configuracoes de Deploy

--- src/app.js
+++ src/app.js
@@ -10,7 +10,8 @@ app.use(router)
 
 
 // Run Server
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 5000
 app.listen(port, () => {
 	console.log(`Servidor rodando na porta ${port}`)
 })
+
